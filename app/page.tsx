1feb553686e807d7722015a10e66b556d4d99855
Psami-wondah <okechukwusamuel16@gmail.com>
2023-11-21 17:58:56 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import LogoGreen from "@/components/icons/logo-green";
+
 export default function Home() {
   return (
     <main className=" font-bold text-5xl text-blue-500 flex items-center justify-center min-h-screen font-mono">
-      WellGab
+      WellGab <LogoGreen />
     </main>
   );
 }
