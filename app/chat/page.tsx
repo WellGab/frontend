3044db513eaa4a25caede255c186c828ab0066ab
Psami-wondah <okechukwusamuel16@gmail.com>
2023-11-27 00:37:06 +0100

    fix responsive hero

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -87,12 +87,12 @@ const Page = () => {
       <NavBar showLinks={false} />
       {isFetching || anonChatMutate.isLoading ? <PageLoader /> : null}
 
-      <div className="px-[6.25rem] font-plusJakartaSans mt-16">
-        <h1 className=" text-[40px] dark:text-transparent bg-clip-text bg-hero-gradient font-semibold text-center">
+      <div className="px-8 lg:px-[6.25rem] font-plusJakartaSans mt-16">
+        <h1 className=" text-3xl lg:text-[40px] dark:text-transparent bg-clip-text bg-hero-gradient font-semibold text-center">
           Hello, I am WellGab. Your AI assistant for all your <br /> symptoms
           and diagnosis questions
         </h1>
-        <div className="flex items-center mt-5 gap-x-2 mx-auto w-max">
+        <div className="flex items-center mt-5 gap-x-2 mx-auto lg:w-max">
           <WarningIcon />
           <p className=" text-[#B4B4B4] dark:text-[#B4B4B4] text-center">
             This is for informational purposes only. Don&apos;t take any actions
@@ -101,9 +101,9 @@ const Page = () => {
         </div>
       </div>
 
-      <div className="bg-[#F5FAFA] dark:bg-[#24252B] mx-[6.25rem] p-8 mt-7 rounded-lg font-plusJakartaSans">
+      <div className="bg-[#F5FAFA] dark:bg-[#24252B] mx-4 lg:mx-[6.25rem] p-4 md:p-8 mt-7 rounded-lg font-plusJakartaSans">
         <div className=" w-full h-full ">
-          <div className="w-full h-[50vh] overflow-y-scroll space-y-6 py-4">
+          <div className="w-full h-[50vh] overflow-y-scroll space-y-6 py-4 text-sm lg:tex-base">
             {messages.map((message, index) => (
               <div key={index}>
                 {message.gpt ? (
@@ -145,7 +145,7 @@ const Page = () => {
             />
 
             <div className=" w-full bg-bg-utilize rounded-xl py-5">
-              <p className=" text-xl font-semibold text-center text-white">
+              <p className=" lg:text-xl font-semibold text-center text-white">
                 You have You&apos;re limited to 10 chats with WellGab. To enjoy
                 unlimited access and <br /> your chat history, Sign up below at
                 no extra charge or hidden cost.
