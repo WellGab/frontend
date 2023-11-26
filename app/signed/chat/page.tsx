f6cfa24b04742904d80b03aba9cea9499b967eb8
Williams <billmal071@gmail.com>
2023-11-26 11:31:33 +0100

    feat: chat page

--- app/signed/chat/page.tsx
+++ app/signed/chat/page.tsx
@@ -21,7 +21,7 @@ function Page() {
           "Hello! I'm here to support with any health-related questions or worries you may have. PLEASE NOTE! The more detailed information you share, the better I can assist you.",
         gpt: true,
       },
-    ]
+    ],
   );
   const activeChatId = useRecoilValue(activeChatIdAtom);
 
@@ -73,29 +73,6 @@ function Page() {
             </p>
           </div>
         )}
-    <main className="flex flex-row gap-4">
-      <SignedSidebar />
-      <div className="flex-[3] flex items-center justify-center flex-col">
-        <Logo width={64} height={64} />
-        <p className="mt-4 text-center font-semibold text-4xl">
-          Hello, I am WellGab. Your AI assistant for all your symptoms and
-          diagnosis questions
-        </p>
-        <form onSubmit={handleSubmit} className="">
-          <input
-            type="text"
-            name="search"
-            id="search"
-            placeholder="Search for symptoms"
-            className="w-[80%] h-10 mt-4 px-4 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          />
-          <button
-            type="submit"
-            className="w-[80%] h-10 mt-4 px-4 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          >
-            Search
-          </button>
-        </form>
       </div>
       <ChatInput onSend={() => {}} />
     </div>
