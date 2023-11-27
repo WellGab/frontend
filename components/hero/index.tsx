079b260c10e4db03f6bd23dcccd181a3d0df2bfc
Psami-wondah <okechukwusamuel16@gmail.com>
2023-11-27 21:19:33 +0100

    feat: subscribe

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -3,10 +3,11 @@ import AI from "@/assets/svgs/ai.svg";
 import HeroImg from "@/assets/imgs/hero-img.png";
 import Image from "next/image";
 import Link from "next/link";
+import { TryWellGab } from "../button";
 
 const Hero = () => {
   return (
-    <section className=" py-[100px] bg-[#F5FAFA] dark:bg-[#202124] h-[150vw] md:h-[90vw] lg:h-[75vw] xl:h-[65vw] 2xl:h-[55vw] text-center relative font-plusJakartaSans">
+    <section className=" py-[100px] bg-[#F5FAFA] dark:bg-[#202124]  h-[650px] md:h-[90vw] lg:h-[75vw] xl:h-[65vw] 2xl:h-[55vw] text-center relative font-plusJakartaSans">
       <div className=" text-4xl md:text-5xl lg:text-6xl xl:text-[64px] font-medium  text-center">
         <h1 className=" flex items-center gap-x-[80px] md:gap-x-[100px] lg:gap-x-[139px] justify-center  relative w-max mx-auto">
           <span className=" dark:text-transparent bg-clip-text bg-hero-gradient">
@@ -33,11 +34,7 @@ const Hero = () => {
         With an AI-Powered Symptom & Diagnosis Guidance at your fingertips; Your
         first step towards informed wellness
       </p>
-      <Link href={"/chat"}>
-        <button className=" mt-12 bg-[#078] w-[320px] h-[60px] rounded-lg text-lg font-sans text-[#F1F3F6] mon-hover">
-          TRY WELLGAB FOR FREE
-        </button>
-      </Link>
+      <TryWellGab />
 
       <div className=" mx-auto flex justify-center -bottom-14  absolute w-full">
         <Image
--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+"use client";
+import { useSubscribe } from "@/hook/subscribe.hook";
+import React, { useState } from "react";
+import { PageLoader } from "../loader";
 
 const Subscribe = () => {
+  const [email, setEmail] = useState("");
+  const { mutate, isLoading } = useSubscribe();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (email.length > 0) {
+      mutate(
+        { email },
+        {
+          onSuccess: () => {
+            setEmail("");
+          },
+        }
+      );
+    }
+  };
   return (
     <section className=" pt-5 bg-[#F5FAFA] dark:bg-[#202124] font-plusJakartaSans pb-20">
+      {isLoading ? <PageLoader /> : null}
       <h2 className=" text-5xl bg-clip-text dark:text-transparent dark:bg-hero-gradient font-medium text-center leading-[56px]">
         Be the first to know
       </h2>
@@ -12,17 +32,25 @@ const Subscribe = () => {
         and news, but no spam, scouts honor!
       </p>
 
-      <div className=" mt-14 rounded-2xl bg-white dark:bg-[#0F0F0F] py-3 px-4 w-[95vw] md:w-[70vw] xl:w-[50vw] flex justify-between items-center mx-auto">
+      <form
+        onSubmit={handleSubmit}
+        className=" mt-14 rounded-2xl bg-white dark:bg-[#0F0F0F] py-3 px-4 w-[95vw] md:w-[70vw] xl:w-[50vw] flex justify-between items-center mx-auto"
+      >
         <input
-          type="text"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Your email address"
           className=" placeholder:text-[#4C4C4C] placeholder:dark:text-[#B4B4B4] bg-transparent outline-none border-none py-2 w-[80%]"
         />
 
-        <button className="  bg-[#078] px-6 py-4 rounded-lg text-lg font-sans text-[#F1F3F6]">
+        <button
+          className="  bg-[#078] px-6 py-4 rounded-lg text-lg font-sans text-[#F1F3F6]"
+          type="submit"
+        >
           Subscribe
         </button>
-      </div>
+      </form>
     </section>
   );
 };
